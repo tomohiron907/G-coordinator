60f34e89469a187d72094aee071f3e1b7e5ef79d
Tomohiro <tomohiron907@gmail.com>
2023-04-21 21:04:10 +0900

    update example

--- example/default_cylinder.py
+++ example/default_cylinder.py
@@ -15,13 +15,15 @@ def object_modeling():
         y = rad*np.sin(arg)
         z = np.full_like(arg, height*print_settings.LAYER+0.2)
         wall = Path(x, y, z)
-        inner_wall = Transform.offset(wall, 0.4)
+        outer_wall = Transform.offset(wall, 0.4)
         full_object.append(wall)
-        full_object.append(inner_wall)
+        full_object.append(outer_wall)
         
         if height <2 :
             bottom = Transform.fill(wall, infill_distance = print_settings.NOZZLE, offset = -print_settings.NOZZLE)
             bottom = Transform.rotate(bottom, np.pi/2*height)
+            bottom.z_hop = True
+            bottom.retraction = True
             full_object.append(bottom)
             
 
--- example/rectangle_wall.py
+++ example/rectangle_wall.py
@@ -2,17 +2,21 @@ import numpy as np
 import math
 from path_generator import *
 
-LAYER=2
+LAYER=20
 
 
 
 def object_modeling():
     full_object=[]
     for height in range(LAYER):
-        x = np.array([10,-10,-10,10,10], dtype = float)
-        y = np.array([10,10,-10,-10,10], dtype = float)
+        x = np.array([10,-10,-10,10], dtype = float)
+        y = np.array([10,10,-10,-10], dtype = float)
         z = np.full_like(x, height*0.2+0.2)
         layer = Path(x, y, z)
+        
+        if height <2 :
+            layer.retraction = True
+            layer.z_hop = True
         full_object.append(layer)
 
     return full_object
