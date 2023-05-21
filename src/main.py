ccf195d80b781e2b4c48684bd51842d943b26c2c
Tomohiro <tomohiron907@gmail.com>
2023-05-21 15:43:26 +0900

    update main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
+'''
+License: MIT
+Author: Tomohiro Taniguchi
+Usage: 
+- Documentation(Japanese): https://qiita.com/tomohiron907/items/747e74965d18f358d852
+- GitHub Repository: https://github.com/tomohiron907/G-coordinator/tree/main
+
+Required Libraries:
+- pyqt5
+- pyqtgraph
+- numpy
+- pyopengl
+- matplotlib
+'''
+
+
+
 import sys
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtPrintSupport import *
 from window.main_window import *
-
-
 
 
 if __name__ == '__main__':
