7fdd248e581292d060af4f634b85db7fe5271e81
Tomohiro <tomohiron907@gmail.com>
2023-06-20 15:22:08 +0900

    update stl slice

--- example/stl_to_gcode.py
+++ example/stl_to_gcode.py
@@ -17,16 +17,10 @@ mesh = trimesh.load(stl_path, merge_norm=True, merge_tex=True)
 def object_modeling():
     full_object=[]
     
-    for height in tqdm(range(LAYER)):
+    for height in range(LAYER):
         wall = slice(mesh, height*0.2)
         full_object.append(wall)
 
-        if height<5:
-            infill = line_infill(wall, density = 1)
-            full_object.append(infill)
-        else:
-            infill = line_infill(wall, density = 0.9, angle = np.pi/2*height+np.pi/4)
-            full_object.append(infill)
         
 
     return full_object
