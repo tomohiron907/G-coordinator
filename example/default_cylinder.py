6aace8ba196a80691d1ba148b32afcd2a28ea7ce
Tomohiro <tomohiron907@gmail.com>
2023-06-28 18:07:12 +0900

    update requirements.txt

--- example/default_cylinder.py
+++ example/default_cylinder.py
@@ -1,25 +1,21 @@
-import numpy as np
 import math
+import numpy as np
 import print_settings 
 from path_generator import *
+from infill_generator import *
 from console import *
 
-
-LAYER =50
+LAYER = 100
 
 def object_modeling():
     full_object=[]
     for height in range(LAYER):
-        arg = np.linspace(0, np.pi*2,100)
-        rad = 10
-        x = rad*np.cos(arg)
-        y = rad*np.sin(arg)
-        z = np.full_like(arg, height*print_settings.layer_height+0.2)
+        arg = np.linspace(0, 2*np.pi, 100)
+        x = 10 * np.cos(arg)
+        y = 10 * np.sin(arg)
+        z = np.full_like(arg, (height+1) * print_settings.layer_height)
         wall = Path(x, y, z)
         full_object.append(wall)
-        print(f'wall at height {height}')
-
-
-
+        print(f'layer at {height=}')
+        
     return full_object
-
