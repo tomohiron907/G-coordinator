d339f49becac65fec10f4eb5762d29b5b0ee0565
Taniguchi Tomohiro <tomohiron907@gmail.com>
2023-11-09 01:37:24 +0900

    update multi axis example;

--- example/kinematics/default_cylinder_bed_rotate.py
+++ example/kinematics/default_cylinder_bed_rotate.py
@@ -6,23 +6,19 @@
 
 
 import numpy as np
+import gcoordinator as gc
 import math
-import print_settings 
-from path_generator import *
-
 LAYER =50
 
-def object_modeling():
-    full_object=[]
-    for height in range(LAYER):
-        rad = 10
-        x = [rad, rad]
-        y = [0, 0]
-        buf_z = height*print_settings.layer_height+0.2
-        z = [buf_z, buf_z]
-        rot = [math.pi*2*height, math.pi*2*(height+1)]
-        wall = Path(x, y, z, rot)
-        full_object.append(wall)        
+full_object=[]
+for height in range(LAYER):
+    rad = 10
+    x = [rad, rad]
+    y = [0, 0]
+    buf_z = height*0.2+0.2
+    z = [buf_z, buf_z]
+    rot = [math.pi*2*height, math.pi*2*(height+1)]
+    wall = gc.Path(x, y, z, rot)
+    full_object.append(wall)        
 
-    return full_object
-
+gc.gui_export(full_object)
--- example/kinematics/default_cylinder_nozzle_tilt.py
+++ example/kinematics/default_cylinder_nozzle_tilt.py
@@ -7,27 +7,20 @@
 
 import numpy as np
 import math
-import print_settings 
-from path_generator import *
-
+import gcoordinator as gc
 LAYER =50
 
-def object_modeling():
-    full_object=[]
-    for height in range(LAYER):
-        arg = np.linspace(0, np.pi*2,100)
-        rad = 10
-        x = rad*np.cos(arg)
-        y = rad*np.sin(arg)
-        z = np.full_like(arg, height*print_settings.layer_height+0.2)
-        rot = arg
-        tilt = [np.pi/2/100*height]*len(x)
-        wall = Path(x, y, z, rot, tilt)
-        full_object.append(wall)
+full_object=[]
+for height in range(LAYER):
+    arg = np.linspace(0, np.pi*2,100)
+    rad = 10
+    x = rad*np.cos(arg)
+    y = rad*np.sin(arg)
+    z = np.full_like(arg, height*0.2+0.2)
+    rot = arg
+    tilt = [np.pi/2/100*height]*len(x)
+    wall = gc.Path(x, y, z, rot, tilt)
+    full_object.append(wall)
 
-
-
-        
-
-    return full_object
+gc.gui_export(full_object)
 
